a1372e14824f8b55757a7fca18e6757c34bfd6ce
PashaLyashok <lyashok_pasha@mail.ru>
2020-06-28 21:50:07 +0300

    add second task

--- script.js
+++ script.js
@@ -9,11 +9,29 @@ slider_left.addEventListener('click',function(event){
     if (left < -5120) left = 0;
    
     slide_l.style.left = left + 'px';
-})
+});
 
 slider_right.addEventListener('click',function(event){
     let slide_r = document.querySelector('.slider_content');
     left = left + 1280;
     if (left > 0) left = -5120;
     slide_r.style.left = left + 'px';
+});
+
+let carousel_left = document.querySelector('.carousel_left'),
+    carousel_right = document.querySelector('.carousel_right'),
+    move = 0;
+
+carousel_left.addEventListener('click',function(event){
+    let carousel_l = document.querySelector('.carousel_content');
+    move = move - 128;
+    if (move < -768) move = 0;
+    carousel_l.style.left = move + 'px';
+});
+
+carousel_right.addEventListener('click',function(event){
+    let carousel_l = document.querySelector('.carousel_content');
+    move = move + 128;
+    if (move > 0) move = -768;
+    carousel_l.style.left = move + 'px';
 })
