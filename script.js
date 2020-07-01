8e86e9e824dc0769f1cd78dda38c1f2889578672
PashaLyashok <lyashok_pasha@mail.ru>
2020-07-01 23:27:46 +0300

    'fixed'

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
 let slider_left = document.querySelector('.slider_left'),
     slider_right = document.querySelector('.slider_right'),
-    left = 0;
+    left = 0,
+    width = screen.width,
+    slider_content = document.querySelector('.slider_content');
+    slide = [];
+
+function addSlide(){
+        slide.push(document.querySelector('.slide1'));
+        slide.push(document.querySelector('.slide2'));
+        slide.push(document.querySelector('.slide3'));
+        slide.push(document.querySelector('.slide4'));
+        slide.push(document.querySelector('.slide5'));
+    
+        for (let i = 0; i < slide.length; i++) {
+            slide[i].style.width = width + 'px';
+        };
+};
+    
+addSlide();
+
+let slider_width = width * 5;
+slider_content.style.width = slider_width + 'px';
+
 
 slider_left.addEventListener('click',function(event){
-    let slide_l = document.querySelector('.slider_content');
-    slide_l.classList.add('slider_content');
-    left = left - 1280;
-    if (left < -5120) left = 0;
-   
-    slide_l.style.left = left + 'px';
+    left = left - width;
+    if (left < -(slider_width - width)) left = 0;
+    slider_content.style.left = left + 'px';
 });
 
 slider_right.addEventListener('click',function(event){
-    let slide_r = document.querySelector('.slider_content');
-    left = left + 1280;
-    if (left > 0) left = -5120;
-    slide_r.style.left = left + 'px';
+    left = left + width;
+    if (left > 0) left = -(slider_width - width);
+    slider_content.style.left = left + 'px';
 });
 
 let carousel_left = document.querySelector('.carousel_left'),
@@ -34,4 +51,4 @@ carousel_right.addEventListener('click',function(event){
     move = move + 128;
     if (move > 0) move = -768;
     carousel_l.style.left = move + 'px';
-})
+});
